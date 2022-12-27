fa271c094622bd01e6577291bcbb563604ff83e9
Katherine Xiong <kxiong22@mit.edu>
2022-12-27 17:41:12 -0500

    added google analytics

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import Projects from './Projects.js';
 import About from './About.js';
 import Map from './Map.js';
 import Photos from './Photos.js';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('G-N4D3N6MEHY');
+ReactGA.pageview(window.location.pathname);
 
 class Main extends React.Component {
     render() {
